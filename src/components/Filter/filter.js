95e8bc0a2c71e50a68c3b93f52b663b3ddc9fa99
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2020-09-15 16:08:37 -0400

    added filter modules from data json file

--- src/components/Filter/filter.js
+++ src/components/Filter/filter.js
@@ -1,27 +1,108 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import './filter.css';
 
 function Filter() {
+  const [moduleFilter, setModuleFilter] = useState([
+    {
+      acronym: "AI",
+      title: "Acquisitions Incorporated",
+      status: true
+    },
+    {
+      acronym: "ALCurseOfStrahd",
+      title: "AL: Curse of Strahd",
+      status: true
+    },
+    {
+      acronym: "ALElementalEvil",
+      title: "AL: Elemental Evil",
+      status: true
+    },
+    {
+      acronym: "ALRageofDemons",
+      title: "AL: Rage of Demons",
+      status: true
+    },
+    {
+      acronym: "BGDIA",
+      title: "Baldur's Gate: Descent Into Avernus",
+      status: true
+    },
+    {
+      acronym: "CoS",
+      title: "Curse of Strahd",
+      status: true
+    },
+    {
+      acronym: "ERLW",
+      title: "Eberron: Rising from the Last War",
+      status: true
+    },
+    {
+      acronym: "EGW",
+      title: "Explorer's Guide to Wildemount",
+      status: true
+    },
+    {
+      acronym: "GoS",
+      title: "Ghosts of Saltmarsh",
+      status: true
+    },
+    {
+      acronym: "GGR",
+      title: "Guildmaster's Guide to Ravnica",
+      status: true
+    },
+    {
+      acronym: "MOT",
+      title: "Mythic Odyssey of Theros",
+      status: true
+    },
+    {
+      acronym: "PHB",
+      title: "Player's Handbook",
+      status: true
+    },
+    {
+      acronym: "SCAG",
+      title: "Sword Coast Adventurer's Guide",
+      status: true
+    },
+    {
+      acronym: "ToA",
+      title: "Tomb of Annihilation",
+      status: true
+    },
+    {
+      acronym: "PSA",
+      title: "PS: Amonkhet",
+      status: true
+    },
+    {
+      acronym: "PSI",
+      title: "PS: Innistrad",
+      status: true
+    }
+  ]);
+  console.log(moduleFilter.filter(module=>module.status));
+  const generateFilterPill = () => {
+    return (
+      <div className="filter-view">
+        {moduleFilter.filter(module => module.status).map((module, index) => {
+          return (
+            <div className="filter-mini-pill" key={index}>
+              {module.title}
+            </div>
+          );
+        })}
+      </div>
+    )
+  }
+
   return (
     <>
       <h3 className="tertiary-header">Filter Sourcebooks</h3>
-      <div className="filter-view">
-        <div className="filter-mini-pill">
-          Acquisitions Incorporated
-        </div>
-        <div className="filter-mini-pill">
-          Baldur's Gate
-        </div>
-        <div className="filter-mini-pill">
-          Curse of Strahd
-        </div>
-        <div className="filter-mini-pill">
-          Eberron
-        </div>
-        <div className="filter-mini-pill">
-          Ravnica
-        </div>
-      </div> 
+      {generateFilterPill()}
     </>
   );
 }
