6bbcfd468158bf3f2ce194bc4a5ba3f168b7b5cb
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2020-09-17 16:22:17 -0400

    loaded background names + feature names

--- src/components/Feature/feature.js
+++ src/components/Feature/feature.js
@@ -1,10 +1,28 @@
 import React from "react"
 import './feature.css';
+import backgrounds from '../../data/backgrounds.json';
 
 function Feature() {
+
+  const generateFeatureList = () => {
+    return (
+      <div className="feature-list">
+        {backgrounds.background.map((background, index) => {
+          return (
+            <div className="background-pill" key={index}>
+              {background.name}: {typeof(background.entries) !== 'undefined' ? background.entries[1].name: ""}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+
   return (
     <>
       <h2 className="secondary-header">Select A Feature</h2>
+      {generateFeatureList()}
     </>
   );
 }
